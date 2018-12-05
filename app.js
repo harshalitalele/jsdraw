1f91bcb4ed4cd4ced360408e90656931c9dcb937
harshalitalele <harshalitalele@gmail.com>
2018-12-05 11:19:40 +0530

    download button added

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ function initiateDrawing() {
     console.log("drawing has been initiated...");
     annoBoard.drawAnnotation();
 }
+
+function download() {
+    var downloadElem = document.getElementById("drawing-board");
+}
