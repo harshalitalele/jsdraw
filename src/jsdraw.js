411d79f49763189f15928c265e8c172bc7a2e7a1
harshalitalele <harshalitalele@gmail.com>
2019-01-21 12:34:24 +0530

    overlay method added

--- src/jsdraw.js
+++ src/jsdraw.js
@@ -13,8 +13,32 @@ function DrawingBoard() {
         clearAllHandler: function() {
             console.log("There is no custom Clear All handler");
         },
-        toolboxStyles: {}
+        toolboxStyles: {},
+        elemProp: {}
     };
+    
+    function getElement(elemProp) {
+        if(elemProp.hasOwnProperty("id")) {
+            return document.getElementById(elemProp.id);
+        } else {
+            return document.body;
+        }
+    }
+    
+    function createOverlay(elemProp) {
+        var overlay = document.createElement("canvas"),
+            element = getElement(elemProp),
+            parentRect = element.getClientRects()[0];
+        overlay.width = parentRect.width;
+        overlay.height = parentRect.height;
+        overlay.style.width = parentRect.width + "px";
+        overlay.style.height = parentRect.height + "px";
+        overlay.style.position = "absolute";
+        overlay.style.top = 0;
+        overlay.style.left = 0;
+        element.appendChild(overlay);
+        return overlay;
+    }
     
     b.Board = function(options) {
         this.options = options || defaultOptions;
@@ -24,5 +48,13 @@ function DrawingBoard() {
         this.saveHandler = this.options.saveHandler;
         this.clearAllHandler = this.options.clearAllHandler;
         this.toolbox = new b.Toolbox();
+        this.showToolbox = function() {
+            //
+        };
+        this.hideToolbox = function() {
+            //
+        };
+        this.canvas = createOverlay(this.options.elemProp);
+        
     }
 }(DrawingBoard));
